665b684e3563969985f28a13a2b2dc31780472e0
Alex140120 <adaniel.rzt@gmail.com>
2025-01-01 20:22:31 -0600

    ADD - Axios

--- rifados-front/src/interfaces/MetodosPago.tsx
+++ rifados-front/src/interfaces/MetodosPago.tsx
@@ -1,91 +1,63 @@
-import MercadoPago from "../assets/img/mercadopago.png";
-import BancoAzteca from "../assets/img/bancoazteca.png";
-import HSBC from "../assets/img/hsbc.jpg";
-import NU from "../assets/img/nu.jpg";
+import instance from "../api/axios";
+import { useEffect, useState } from "react";
+
+interface CuentasBancariasI {
+  banco: string;
+  clabe: string;
+  imagenbanco: string;
+  no_tarjeta: string;
+  titularCuenta: string;
+}
 
 export default function MetodosPago() {
+  const [cuentasBancarias, setCuentasBancarias] = useState<CuentasBancariasI[]>(
+    []
+  );
+
+  useEffect(() => {
+    cuentasBancariasFunction();
+  }, []);
+
+  const cuentasBancariasFunction = () => {
+    instance
+      .get("/cuentasBancarias")
+      .then(function (response) {
+        // handle success
+        //console.log(response.data);
+        setCuentasBancarias(response.data.cuentas);
+      })
+      .catch(function (error) {
+        // handle error
+        console.log(error);
+      });
+  };
+
   return (
     <div>
       <div className="w-100 mt-4 pb-5 px-2">
-        <div className="row m-0 rounded-3 p-2 datos-banco mb-3 py-2">
-          <div className="col-lg-2 col-md-12 col-xs-12 border-end d-flex align-items-center justify-content-center">
-            <img src={HSBC} className="img-carusel rounded-2" />
-          </div>
-          <div className="col-lg-10 col-md-12 col-xs-12 mt-2">
-            <div className="d-flex mb-1" style={{ fontSize: "15px" }}>
-              <p className="m-0 w-25">Nombre:</p>
-              <p className="m-0 w-75">Nombre del titular de la cuenta</p>
-            </div>
-            <div className="d-flex mb-1" style={{ fontSize: "15px" }}>
-              <p className="m-0 w-25">Clabe:</p>
-              <p className="m-0 w-75">12542154787</p>
-            </div>
-            <div className="d-flex" style={{ fontSize: "15px" }}>
-              <p className="m-0 w-25">No. Tarjeta:</p>
-              <p className="m-0 w-75">12457845</p>
-            </div>
-          </div>
-        </div>
-
-        <div className="row m-0 rounded-3 p-2 datos-banco mb-3 py-2">
-          <div className="col-lg-2 col-md-12 col-xs-12 border-end d-flex align-items-center justify-content-center">
-            <img src={BancoAzteca} className="img-carusel rounded-2" />
-          </div>
-          <div className="col-lg-10 col-md-12 col-xs-12 mt-2">
-            <div className="d-flex mb-1" style={{ fontSize: "15px" }}>
-              <p className="m-0 w-25">Nombre:</p>
-              <p className="m-0 w-75">Nombre del titular de la cuenta</p>
-            </div>
-            <div className="d-flex mb-1" style={{ fontSize: "15px" }}>
-              <p className="m-0 w-25">Clabe:</p>
-              <p className="m-0 w-75">12542154787</p>
-            </div>
-            <div className="d-flex" style={{ fontSize: "15px" }}>
-              <p className="m-0 w-25">No. Tarjeta:</p>
-              <p className="m-0 w-75">12457845</p>
-            </div>
-          </div>
-        </div>
-
-        <div className="row m-0 rounded-3 p-2 datos-banco mb-3 py-2">
-          <div className="col-lg-2 col-md-12 col-xs-12 border-end d-flex align-items-center justify-content-center">
-            <img src={MercadoPago} className="img-carusel rounded-2" />
-          </div>
-          <div className="col-lg-10 col-md-12 col-xs-12 mt-2">
-            <div className="d-flex mb-1" style={{ fontSize: "15px" }}>
-              <p className="m-0 w-25">Nombre:</p>
-              <p className="m-0 w-75">Nombre del titular de la cuenta</p>
-            </div>
-            <div className="d-flex mb-1" style={{ fontSize: "15px" }}>
-              <p className="m-0 w-25">Clabe:</p>
-              <p className="m-0 w-75">12542154787</p>
-            </div>
-            <div className="d-flex" style={{ fontSize: "15px" }}>
-              <p className="m-0 w-25">No. Tarjeta:</p>
-              <p className="m-0 w-75">12457845</p>
-            </div>
-          </div>
-        </div>
-
-        <div className="row m-0 rounded-3 p-2 datos-banco mb-3 py-2">
-          <div className="col-lg-2 col-md-12 col-xs-12 border-end d-flex align-items-center justify-content-center">
-            <img src={NU} className="img-carusel rounded-2" />
-          </div>
-          <div className="col-lg-10 col-md-12 col-xs-12 mt-2">
-            <div className="d-flex mb-1" style={{ fontSize: "15px" }}>
-              <p className="m-0 w-25">Nombre:</p>
-              <p className="m-0 w-75">Nombre del titular de la cuenta</p>
-            </div>
-            <div className="d-flex mb-1" style={{ fontSize: "15px" }}>
-              <p className="m-0 w-25">Clabe:</p>
-              <p className="m-0 w-75">12542154787</p>
-            </div>
-            <div className="d-flex" style={{ fontSize: "15px" }}>
-              <p className="m-0 w-25">No. Tarjeta:</p>
-              <p className="m-0 w-75">12457845</p>
-            </div>
-          </div>
-        </div>
+        {cuentasBancarias.length
+          ? cuentasBancarias.map((cuenta, index) => (
+              <div key={index} className="row m-0 rounded-3 p-2 datos-banco mb-3 py-2">
+                <div className="col-lg-2 col-md-12 col-xs-12 border-end d-flex align-items-center justify-content-center">
+                  <img src={cuenta.imagenbanco} className="img-carusel rounded-2" />
+                </div>
+                <div className="col-lg-10 col-md-12 col-xs-12 mt-2">
+                  <div className="d-flex mb-1" style={{ fontSize: "15px" }}>
+                    <p className="m-0 w-25">Nombre:</p>
+                    <p className="m-0 w-75">{cuenta.titularCuenta}</p>
+                  </div>
+                  <div className="d-flex mb-1" style={{ fontSize: "15px" }}>
+                    <p className="m-0 w-25">Clabe:</p>
+                    <p className="m-0 w-75">{cuenta.clabe}</p>
+                  </div>
+                  <div className="d-flex" style={{ fontSize: "15px" }}>
+                    <p className="m-0 w-25">No. Tarjeta:</p>
+                    <p className="m-0 w-75">{cuenta.no_tarjeta}</p>
+                  </div>
+                </div>
+              </div>
+            ))
+          : null}
       </div>
     </div>
   );

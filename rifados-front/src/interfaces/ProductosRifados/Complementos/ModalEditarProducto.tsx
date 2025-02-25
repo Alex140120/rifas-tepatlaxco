55500d8cdc297577df17797fd3f02f7f2f57f644
Alex140120 <adaniel.rzt@gmail.com>
2025-02-25 00:06:04 -0600

    ADD - Prodcutos
    Vista Previa de imagen del producto

--- rifados-front/src/interfaces/ProductosRifados/Complementos/ModalEditarProducto.tsx
+++ rifados-front/src/interfaces/ProductosRifados/Complementos/ModalEditarProducto.tsx
@@ -2,7 +2,11 @@ import { Modal } from "react-bootstrap";
 import InputText from "../../../components/Tags/InputText";
 import TextArea from "../../../components/Tags/TextArea";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faSave, faTimes } from "@fortawesome/free-solid-svg-icons";
+import {
+  faArrowAltCircleLeft,
+  faSave,
+  faTimes,
+} from "@fortawesome/free-solid-svg-icons";
 import { useEffect, useState } from "react";
 import {
   notifyError,
@@ -46,6 +50,8 @@ export default function ModalEditarProducto({
   const [archivos, setArchivos] = useState<ImagenesI[]>([]);
   const [cantidadBoletos, setCantidadBoletos] = useState<string>("");
 
+  const [rutaImagen, setRutaImagen] = useState<string>("");
+
   useEffect(() => {
     if (show) {
       console.log(productoSeleccionado);
@@ -59,6 +65,7 @@ export default function ModalEditarProducto({
       setNombreProducto("");
       setDescripcionProducto("");
       setCantidadBoletos("");
+      setRutaImagen("");
     }
   }, [show]);
 
@@ -101,6 +108,8 @@ export default function ModalEditarProducto({
   const eliminarArchivo = async (idimage: number) => {
     const nuevosArchivos = archivos.filter((item) => item.idimage !== idimage);
     setArchivos(nuevosArchivos);
+
+    console.log(idimage);
   };
 
   const RecortarTexto = (texto: string, limite: number) => {
@@ -109,6 +118,11 @@ export default function ModalEditarProducto({
       texto.length > limite ? texto.substring(0, limite) + "..." : texto;
 
     return textoRecortado;
+  };
+
+  const vistaPreviaImagen = (ruta: string) => {
+    console.log(ruta);
+    setRutaImagen(ruta);
   };
 
   return (
@@ -127,95 +141,126 @@ export default function ModalEditarProducto({
         </Modal.Header>
         <Modal.Body>
           <div className="w-100">
-            <div>
-              <div className="w-100">
-                <InputText
-                  disabled={false}
-                  placeHolder="Nombre del Producto"
-                  valor={nombreProducto}
-                  tipoValor="texto"
-                  onChange={(e) => setNombreProducto(e)}
-                />
-              </div>
-              <div className="col-lg-12 col-md-12 col-sm-12 mt-3">
-                <TextArea
-                  disabled={false}
-                  placeHolder="Descripción del producto"
-                  valor={descripcionProducto}
-                  onChange={(e) => setDescripcionProducto(e.target.value)}
-                />
-              </div>
+            {rutaImagen === "" ? (
+              <div>
+                <div className="w-100">
+                  <InputText
+                    disabled={false}
+                    placeHolder="Nombre del Producto"
+                    valor={nombreProducto}
+                    tipoValor="texto"
+                    onChange={(e) => setNombreProducto(e)}
+                  />
+                </div>
+                <div className="col-lg-12 col-md-12 col-sm-12 mt-3">
+                  <TextArea
+                    disabled={false}
+                    placeHolder="Descripción del producto"
+                    valor={descripcionProducto}
+                    onChange={(e) => setDescripcionProducto(e.target.value)}
+                  />
+                </div>
 
-              <div className="col-lg-12 col-md-12 col-sm-12">
-                {archivos.length > 0 ? (
-                  <div className="mt-0">
-                    <table className="t3 files-table">
-                      <thead>
-                        <tr>
-                          <th>No.</th>
-                          <th>Nombre</th>
-                          <th className="text-center">Eliminar</th>
-                        </tr>
-                      </thead>
-                      <tbody>
-                        {archivos.map((file, index) => (
-                          <tr key={index}>
-                            <td>{index + 1}</td>
-                            <td>
-                              <span className="t3 p-0">
-                                {RecortarTexto(file.nombrearchivo, 20)}
-                              </span>
-                            </td>
-                            <td>
-                              <div className="w-100 d-flex">
-                                <button
-                                  className="btn-danger-rifas m-auto rounded-1 t5"
-                                  onClick={() => {                                    
-                                    eliminarArchivo(file.idimage);
-                                  }}
-                                >
-                                  <FontAwesomeIcon icon={faTimes} />
-                                </button>
-                              </div>
-                            </td>
+                <div className="col-lg-12 col-md-12 col-sm-12">
+                  {archivos.length > 0 ? (
+                    <div className="mt-0">
+                      <table className="t3 files-table">
+                        <thead>
+                          <tr>
+                            <th>No.</th>
+                            <th>Nombre</th>
+                            <th></th>
+                            <th className="text-center">Eliminar</th>
                           </tr>
-                        ))}
-                      </tbody>
-                    </table>
-                  </div>
-                ) : (
-                  <div className="w-100 mt-2">
-                    <span className="t3">No hay archivos almacenados</span>
-                  </div>
-                )}
-              </div>
+                        </thead>
+                        <tbody>
+                          {archivos.map((file, index) => (
+                            <tr key={index}>
+                              <td>{index + 1}</td>
+                              <td>
+                                <span className="t3 p-0">
+                                  {RecortarTexto(file.nombrearchivo, 20)}
+                                </span>
+                              </td>
+                              <td>
+                                <a
+                                  className="text-primary"
+                                  style={{
+                                    cursor: "pointer",
+                                    textDecoration: "underline",
+                                  }}
+                                  onClick={() => vistaPreviaImagen(file.ruta)}
+                                >
+                                  Vista previa
+                                </a>
+                              </td>
+                              <td>
+                                <div className="w-100 d-flex">
+                                  <button
+                                    className="btn-danger-rifas m-auto rounded-1 t5 text-light"
+                                    onClick={() => {
+                                      eliminarArchivo(file.idimage);
+                                    }}
+                                  >
+                                    <FontAwesomeIcon icon={faTimes} />
+                                  </button>
+                                </div>
+                              </td>
+                            </tr>
+                          ))}
+                        </tbody>
+                      </table>
+                    </div>
+                  ) : (
+                    <div className="w-100 mt-2">
+                      <span className="t3">No hay archivos almacenados</span>
+                    </div>
+                  )}
+                </div>
 
-              <div className="col-lg-12 col-md-12 col-sm-12 mt-3">
-                <InputText
-                  disabled={false}
-                  placeHolder="Cantidad de Boletos"
-                  valor={cantidadBoletos}
-                  tipoValor="numero"
-                  onChange={(valor) => setCantidadBoletos(valor)}
-                />
-              </div>
+                <div className="col-lg-12 col-md-12 col-sm-12 mt-3">
+                  <InputText
+                    disabled={false}
+                    placeHolder="Cantidad de Boletos"
+                    valor={cantidadBoletos}
+                    tipoValor="numero"
+                    onChange={(valor) => setCantidadBoletos(valor)}
+                  />
+                </div>
 
-              <div className="w-100 d-flex justify-content-center align-items-center mt-3">
-                <button
-                  className="t3 btn-primary-rifas border rounded-2 p-2 text-light"
-                  disabled={false}
-                  onClick={() => {
-                    handleGuardarCambios();
-                  }}
-                >
-                  <FontAwesomeIcon icon={faSave} className="me-1" />
-                  Guardar Cambios
-                </button>
+                <div className="w-100 d-flex justify-content-center align-items-center mt-3">
+                  <button
+                    className="t3 btn-primary-rifas border rounded-2 p-2 text-light"
+                    disabled={false}
+                    onClick={() => {
+                      handleGuardarCambios();
+                    }}
+                  >
+                    <FontAwesomeIcon icon={faSave} className="me-1" />
+                    Guardar Cambios
+                  </button>
+                </div>
               </div>
-            </div>
+            ) : (
+              <div>
+                <img src={rutaImagen} className="w-100" />
+              </div>
+            )}
           </div>
         </Modal.Body>
-        <Modal.Footer></Modal.Footer>
+        <Modal.Footer>
+          <div className="w-100 d-flex justify-content-center align-items-center mt-3">
+            {rutaImagen !== "" ? (
+              <button
+                className="bg-light rounded-1 text-dark border t3 p-2 me-3"
+                onClick={() => setRutaImagen("")}
+              >
+                <FontAwesomeIcon icon={faArrowAltCircleLeft} className="me-1" />
+                Atrás
+              </button>
+            ) : null}
+          </div>
+        </Modal.Footer>
       </Modal>
     </div>
   );
